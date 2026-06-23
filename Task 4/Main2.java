b1aa6f82d2620d50bb4c2ebd8e5f801e0ba75e66
Sadia905 <sadiasharmin905@gmail.com>
2026-06-23 11:18:19 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,30 @@
+abstract class TeamMember {
+    protected String name;
+    public TeamMember(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}             
+interface BonusEligible {
+    void calculateBonus();
+}
 
+class Employee extends TeamMember implements BonusEligible {
+    public Employee(String name) {
+        super(name);
+    }
+    public void calculateBonus(){
+        System.out.println("Calculating standerd employee bonus...");
+    }
+}
+class Contractor extends TeamMember {
+    public Contractor(String name) {
+        super(name);
+    }
+    
+    public void performWork() {
+        System.out.println(name + " is working as a contractor.");
+    }
+    
